9ae88ada1aa0b6e69c302fded602e26985b254cf
RUDepure <alexrum92@gmail.com>
2021-10-12 14:12:03 -0500

    initial commit

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string option = "";
+
+            do
+            {
+                Console.WriteLine("What type of conversion do you want to do?");
+                Console.WriteLine("Farenheit to Celsius = ftc");
+                Console.WriteLine("Celsius to Farenheit = ctf");
+                Console.WriteLine("Exit = None");
+                bool parseSuccess = false;
+                double tempValue = 0;
+                double result = 0;
+
+                option = Console.ReadLine();
+
+                switch (option.ToLower())
+                {
+                    case "ftc":
+
+                        Console.Write("Enter the value Farenheit value you want to convert to Celsius:");
+                        parseSuccess = double.TryParse(Console.ReadLine(), out tempValue);
+
+                        while (parseSuccess == false)
+                        {
+                            Console.WriteLine("Please enter a valid numeric value\n");
+                            parseSuccess = double.TryParse(Console.ReadLine(), out tempValue);
+                        }
+
+                        result = TempConverter.FahrenheitToCelsius(tempValue);
+                        Console.WriteLine($"{tempValue} °F = {result} °C");
+                        Console.WriteLine($"============================================\n");
+
+                        break;
+
+                    case "ctf":
+
+                        Console.Write("Enter the value Celsius value you want to convert to Farenheit:");
+                        parseSuccess = double.TryParse(Console.ReadLine(), out tempValue);
+
+                        while (parseSuccess == false)
+                        {
+                            Console.WriteLine("Please enter a valid numeric value\n");
+                            parseSuccess = double.TryParse(Console.ReadLine(), out tempValue);
+                        }
+
+                        result = TempConverter.CelsiusToFahrenheit(tempValue);
+                        Console.WriteLine($"{tempValue} °C = {result} °F");
+                        Console.WriteLine($"============================================\n");
+
+                        break;
+
+                    case "none":
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter one of the specified options.");
+                        Console.WriteLine($"============================================\n");
+                        break;
+                }
+
+            } while (option != "none");
+        }
+    }
+}
---
+++ StaticKeywordExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticKeywordExercise
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double temp)
+        {
+            var result = (temp - 32) / 1.8;
+
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit(double temp)
+        {
+            var result = (temp * 9) / 5 + 32;
+
+            return result;
+        }
+    }
+}
